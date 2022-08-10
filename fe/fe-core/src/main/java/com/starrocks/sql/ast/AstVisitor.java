73ad751760024acca33cbdecfcffb4d909e89e92
fzhedu <fzhedu@gmail.com>
2022-08-10 10:32:20 +0800

    add lambda function of transform(array, lambda expr)
    Signed-off-by: fzhedu <fzhedu@gmail.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/ast/AstVisitor.java
@@ -46,6 +46,8 @@ import com.starrocks.analysis.InformationFunction;
 import com.starrocks.analysis.InsertStmt;
 import com.starrocks.analysis.IsNullPredicate;
 import com.starrocks.analysis.KillStmt;
+import com.starrocks.analysis.LambdaArguments;
+import com.starrocks.analysis.LambdaFunction;
 import com.starrocks.analysis.LikePredicate;
 import com.starrocks.analysis.LimitElement;
 import com.starrocks.analysis.LiteralExpr;
@@ -754,7 +756,13 @@ public abstract class AstVisitor<R, C> {
     public R visitLikePredicate(LikePredicate node, C context) {
         return visitExpression(node, context);
     }
+    public R visitLambdaFunction(LambdaFunction node, C context) {
+        return visitExpression(node, context);
+    }
 
+    public R visitLambdaArguments(LambdaArguments node, C context) {
+        return visitExpression(node, context);
+    }
     public R visitLiteral(LiteralExpr node, C context) {
         return visitExpression(node, context);
     }
