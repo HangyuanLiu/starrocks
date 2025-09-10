79bc898fae9b32722eaf202cc8733024586d03b4
Harbor Liu <460660596@qq.com>
2025-09-10 17:19:58 +0800

    stage4

---
+++ fe/fe-core/src/main/java/com/starrocks/qe/SessionConnectionContext.java
@@ -0,0 +1,64 @@
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.starrocks.qe;
+
+import com.starrocks.mysql.MysqlChannel;
+
+import java.util.UUID;
+
+/**
+ * SessionConnectionContext encapsulates connection channel and identifiers for a session
+ * (mysqlChannel, remoteIP, sessionId, connectionId).
+ */
+public class SessionConnectionContext {
+    private MysqlChannel mysqlChannel;
+    private String remoteIP;
+    private UUID sessionId;
+    private int connectionId;
+
+    public MysqlChannel getMysqlChannel() {
+        return mysqlChannel;
+    }
+
+    public void setMysqlChannel(MysqlChannel mysqlChannel) {
+        this.mysqlChannel = mysqlChannel;
+    }
+
+    public String getRemoteIP() {
+        return remoteIP;
+    }
+
+    public void setRemoteIP(String remoteIP) {
+        this.remoteIP = remoteIP;
+    }
+
+    public UUID getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(UUID sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public int getConnectionId() {
+        return connectionId;
+    }
+
+    public void setConnectionId(int connectionId) {
+        this.connectionId = connectionId;
+    }
+}
+
+
